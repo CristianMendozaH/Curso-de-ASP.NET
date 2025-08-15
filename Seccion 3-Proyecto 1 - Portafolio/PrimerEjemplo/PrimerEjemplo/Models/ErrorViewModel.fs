9136262f753e0d068c796c7ccc8b69cecc191f8e
Cristian Mendoza <cristian8mendoza8@gmail.com>
2025-08-14 21:04:24 -0600

    Progreso en casa

---
+++ Seccion 3-Proyecto 1 - Portafolio/PrimerEjemplo/PrimerEjemplo/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace PrimerEjemplo.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
